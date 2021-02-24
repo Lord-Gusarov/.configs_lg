7f8eb2df656742a22b6d78989aead7478568c89a
Gustavo Hornedo <gustavo.hc18@gmail.com>
2021-02-24 06:20:45 +0000

    Adds a git set-up script

---
+++ git_setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Setups Git as if it was the first time being used
+
+# Install git if not present
+sudo apt install git
+# Sets username, email and persistent id with store
+git config --global user.name "Gustavo Hornedo"
+git config --global user.email "gustavo.hc18@gmail.com"
+git config credential.helper store
+# Clones common Holberton's Repos into new folder
+mkdir ~/hrepos
+cd ~/hrepos
+git clone https://github.com/Lord-Gusarov/holberton-system_engineering-devops.git
+git clone https://github.com/Lord-Gusarov/holbertonschool-low_level_programming.git
+git clone https://github.com/Lord-Gusarov/holbertonschool-higher_level_programming.git
