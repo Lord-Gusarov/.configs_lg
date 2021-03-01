fd11a07527a12d58beeac07b5f1e6b86c5e9dc05
Gustavo Hornedo <gustavo.hc18@gmail.com>
2021-03-01 15:41:52 +0000

    Specifically msql 5.7

--- hlb_setup.sh
+++ hlb_setup.sh
@@ -5,5 +5,7 @@ cd bettyLinter
 cd ..
 #Sets up Pep8 version 1.7.0
 ./pep8/install.sh
-#Install MySQL
-apt-get install myswl-server
+#Install MySQL 5.7 on Ubuntu 14.04 LTS
+echo 'deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7-dmr' | sudo tee -a /etc/apt/sources.list
+apt-get update
+apt-get install mysql-server-5.7 -y
