ca195b6321600d11c3aba6ffab8260b58f0309a0
Gustavo Hornedo <gustavo.hc18@gmail.com>
2021-03-23 10:10:55 -0400

    Node 10 and style validator,, setup

---
+++ node10_setup.sh
@@ -0,0 +1,5 @@
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Tool to check complaince with Rules of Standars + semiconlons on top
+sudo npm install semistandard --global
