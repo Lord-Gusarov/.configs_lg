4c598be675963a2d084cd9eed7ed189c2810f17d
Gustavo Hornedo <70659799+Lord-Gusarov@users.noreply.github.com>
2021-05-28 06:45:13 -0400

    Using SSH for all cloning, dir hrepos created only if it does not exist and adds more common repos

--- git_setup.sh
+++ git_setup.sh
@@ -6,9 +6,15 @@ git config --global user.name "Gustavo Hornedo"
 git config --global user.email "gustavo.hc18@gmail.com"
 git config credential.helper store
 # Clones common Holberton's Repos into new folder
-mkdir ~/hrepos
+mkdir -p ~/hrepos
 cd ~/hrepos
-git clone https://github.com/Lord-Gusarov/holberton-system_engineering-devops.git
-git clone https://github.com/Lord-Gusarov/holbertonschool-low_level_programming.git
-git clone https://github.com/Lord-Gusarov/holbertonschool-higher_level_programming.git
-git clone https://github.com/Lord-Gusarov/AirBnB_clone.git
+git clone git@github.com:Lord-Gusarov/holberton-system_engineering-devops.git
+git clone git@github.com:Lord-Gusarov/Fix_My_Code_Challenge.git
+git clone git@github.com:Lord-Gusarov/AirBnB_clone_v4.git
+git clone git@github.com:Lord-Gusarov/holbertonschool-higher_level_programming.git
+git clone git@github.com:Lord-Gusarov/AirBnB_clone_v3.git
+git clone git@github.com:Lord-Gusarov/holbertonschool-low_level_programming.git
+git clone git@github.com:Lord-Gusarov/binary_trees.git
+git clone git@github.com:Lord-Gusarov/sorting_algorithms.git
+git clone git@github.com:Lord-Gusarov/tic-tac-toe.git
+git clone git@github.com:Lord-Gusarov/monty.git
