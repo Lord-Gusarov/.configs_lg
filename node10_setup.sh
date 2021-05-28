e27add60f5938e6f9bb4b44a46c168ac7f58cdc1
Gustavo Hornedo <70659799+Lord-Gusarov@users.noreply.github.com>
2021-05-28 09:14:04 -0400

    Update node10_setup.sh

--- node10_setup.sh
+++ node10_setup.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs --force-yes
 
 # Tool to check complaince with Rules of Standars + semiconlons on top
 sudo npm install semistandard --global
